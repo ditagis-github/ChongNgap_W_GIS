dcb0eb6b8a7abc494269c546226238c5da4af267
HongNguyen <hongnguyen060895@gmail.com>
2018-07-27 09:40:56 +0700

    chuyển file css thành một folder

--- WebTNBDGIS/App_Start/BundleConfig.cs
+++ WebTNBDGIS/App_Start/BundleConfig.cs
@@ -26,7 +26,7 @@ namespace WebTNBDGIS
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap/css/bootstrap.min.css",
                       "~/Content/font-awesome-4.7.0/css/font-awesome.min.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/site.css"));
         }
     }
 }
