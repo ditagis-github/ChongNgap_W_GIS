4a035216e60e67cf081efc162ae7e409bb70f8e3
HongNguyen <hongnguyen060895@gmail.com>
2018-11-13 18:54:37 +0700

    ban đồ ngập

---
+++ WebTNBDGIS/Scripts/ditagis/bandongap.js
@@ -0,0 +1,45 @@
+﻿require([
+    "esri/Map",
+    "esri/views/MapView",
+    "esri/layers/ImageryLayer",
+    "esri/layers/support/RasterFunction",
+    "esri/layers/support/MosaicRule",
+    "dojo/domReady!"
+], function (Map, MapView, ImageryLayer,
+    RasterFunction, MosaicRule
+
+
+) {
+
+        var colorRF = new RasterFunction({
+            functionName: "Stretched",
+            variableName: "Raster"
+        });
+
+        var layer = new ImageryLayer({
+            url: "http://ditagis.com/arcgis/rest/services/BD_ChongNgap/N/ImageServer",
+            renderingRule: colorRF,
+        });
+
+        /*************************
+         * Add image layer to map
+         ************************/
+
+        var map = new Map({
+            basemap: "gray",
+            layers: [layer]
+        });
+
+        var view = new MapView({
+            container: "viewDiv",
+            map: map,
+            center: [106, 11],
+            zoom: 9
+        });
+
+        document.getElementById('submit').onclick = function () {
+            layer.definitionExpression = `Name = '${document.getElementById('definition').value}'`
+            layer.refresh();
+        }
+
+    });
