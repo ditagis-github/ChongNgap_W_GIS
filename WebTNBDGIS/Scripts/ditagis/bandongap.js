6cf330f0ecc3aea4a370313094fea9241848240c
HongNguyen <hongnguyen060895@gmail.com>
2018-12-07 16:20:38 +0700

    cập nhật basemap bản đồ ngập

--- WebTNBDGIS/Scripts/ditagis/bandongap.js
+++ WebTNBDGIS/Scripts/ditagis/bandongap.js
@@ -1,27 +1,80 @@
 ﻿require([
     "esri/Map",
     "esri/views/MapView",
+    "esri/layers/FeatureLayer",
+    "esri/layers/MapImageLayer",
     "esri/layers/ImageryLayer",
     "esri/layers/support/RasterFunction",
-    "esri/layers/support/MosaicRule",
     "esri/core/watchUtils",
     "esri/widgets/Legend",
+    "esri/widgets/LayerList",
+    "esri/widgets/Expand",
     "dojo/domReady!"
-], function (Map, MapView, ImageryLayer,
-    RasterFunction, MosaicRule, watchUtils,Legend
+], function (Map, MapView, FeatureLayer, MapImageLayer, ImageryLayer,
+    RasterFunction, watchUtils, Legend, LayerList,Expand
 
 
 ) {
-
         var colorRF = new RasterFunction({
             functionName: "Stretched",
             variableName: "Raster"
         });
+        var color1 = "rgb(191, 255, 232)";
+        var color2 = "rgb(108,247,255)";
+        var color3 = "rgb(44,180,230)";
+        var color4 = "rgb(0,129,255)";
+        var color5 = "rgb(0,0,255)";
+        var color6 = "rgb(0,0,141)";
 
-        var layer = new ImageryLayer({
+        var valueExpression = getExpressionValues("D01");
+        var renderer = {
+            type: "unique-value",  // autocasts as new UniqueValueRenderer()
+            field: "D01",
+            valueExpression: valueExpression,
+            uniqueValueInfos: [{
+                value: "1",
+                symbol: getSymbol(color1),
+                label: "0.0 - 0.1"
+            }, {
+                value: "2",
+                symbol: getSymbol(color2),
+                label: "0.1 - 0.2"
+            }, {
+                value: "3",
+                symbol: getSymbol(color3),
+                label: "0.2 - 0.5"
+            }, {
+                value: "4",
+                symbol: getSymbol(color4),
+                label: "0.5 - 1.0"
+            }, {
+                value: "5",
+                symbol: getSymbol(color5),
+                label: "1.0 - 2.0"
+            }, {
+                value: "6",
+                symbol: getSymbol(color6),
+                label: "2.0 - 3.0"
+            }]
+        };
+        var tuyenDuongNgap_IL = new FeatureLayer({
+            url: "http://ditagis.com/arcgis/rest/services/BD_ChongNgap/Ngap1D/MapServer/0",
+            title: "Mức độ tuyến đường ngập",
+            visible: false,
+            renderer: renderer
+        });
+
+        var mucDoVungNgap_IL = new ImageryLayer({
             url: "http://ditagis.com/arcgis/rest/services/BD_ChongNgap/N/ImageServer",
             renderingRule: colorRF,
-            definitionExpression: "Name = 'N00'"
+            definitionExpression: "Name = 'N01'",
+            title: "Mức độ vùng ngập",
+        });
+
+        var baseMap_MIL = new MapImageLayer({
+            url: "https://ditagis.com/arcgis/rest/services/BD_ChongNgap/BaseMap_BanDoNgap/MapServer",
+            visible: false,
+            title: "Bản đồ nền",
         });
 
         /*************************
@@ -30,25 +83,38 @@
 
         var map = new Map({
             basemap: "gray",
-            layers: [layer]
+            layers: [baseMap_MIL, tuyenDuongNgap_IL, mucDoVungNgap_IL]
         });
 
         var view = new MapView({
-            container: "viewDiv",
+            container: "mapDiv",
             map: map,
-            center: [106, 11],
-            zoom: 9
+            center: [106.6276006, 11.2120527],
+            ui: { components: [] },
+            zoom: 10
         });
         var legend = new Legend({
             view: view,
             layerInfos: [{
-              layer: layer,
-              title: "Legend"
-            }]
-          });
-          
-          view.ui.add(legend, "top-right");
-        layer.on("layerview-create", (event) => {
+                layer: tuyenDuongNgap_IL,
+            }, {
+                layer: mucDoVungNgap_IL,
+            }],
+        });
+
+        view.ui.add(legend, "top-right");
+
+        view.when(function () {
+            var layerList = new LayerList({
+                view: view
+            });
+            var expand = new Expand({
+                view: view,
+                content: layerList
+            });
+            view.ui.add(expand, "top-left");
+        });
+        mucDoVungNgap_IL.on("layerview-create", (event) => {
             // The LayerView for the layer that emitted this event
             watchUtils.watch(event.layerView, 'updating', (newVal, oldVal) => {
                 var element = $("#timelineContainer");
@@ -58,6 +124,29 @@
             })
 
         });
+        function getSymbol(color) {
+            var symbol = {
+                type: "simple-line",  // autocasts as new SimpleLineSymbol()
+                color: color,
+                width: 1,
+                miterLimit: 7.5,
+                style: "solid",
+                cap: "round"
+            };
+            return symbol;
+        }
+        function getExpressionValues(field) {
+            var attr = `$feature.${field}`;
+            var ValueInfo1 = `(${attr} >= 0 && ${attr} <= 0.1),'1'`;
+            var ValueInfo2 = `${attr} > 0.1 && ${attr} <= 0.2,'2'`;
+            var ValueInfo3 = `${attr} > 0.2 && ${attr} <= 0.5,'3'`;
+            var ValueInfo4 = `${attr} > 0.5 && ${attr} <= 1,'4'`;
+            var ValueInfo5 = `${attr} > 1 && ${attr} <= 2,'5'`;
+            var ValueInfo6 = `${attr} > 2 && ${attr} <= 3,'6'`;
+            var valueExpression = `When(${ValueInfo1},${ValueInfo2},${ValueInfo3},${ValueInfo4},${ValueInfo5},${ValueInfo6},'7')`;
+            return valueExpression;
+
+        }
         function GetDataQuery() {
             var datas = [];
             var cur_Day = new Date();
@@ -94,15 +183,18 @@
         function InitializeTimeline() {
             // Set timeline width based on number of months spanned adding one month for buffer
             var datas = GetDataQuery();
-            var width = ($(window).width() - 150)/24;
+            var width = ($(window).width() - 150) / 24;
+
             for (const index in datas) {
+                let class_Name = 'event-marker';
                 var data = datas[index];
                 var i = parseInt(index);
                 $("#ticks").append(
                     "<li class='timeline-tick' style='left: " + ((i * width) + 50) + "px'><span class='tick-label'>" + data.Time + "</span></li>"
                 );
+                if (i == 0) class_Name += ' selected';
                 eventMarker =
-                    "<li class='event-marker" +
+                    "<li class='" + class_Name +
                     "' style='left: " +
                     ((i * width) + 46) +
                     "px' title='" +
@@ -117,6 +209,7 @@
 
         function AddEventHandlers() {
             $(".event-marker").click(function () {
+                if (!mucDoVungNgap_IL.visible) return;
                 // Change marker
                 $(".event-marker").removeClass("selected");
                 $(this).addClass("selected");
@@ -124,9 +217,10 @@
                 kendo.ui.progress(element, true);
                 // Show event detail
                 var eventId = $(this).attr("data-id");
-                    var definitionExpression = `Name = 'N${eventId}'`;
-                    layer.definitionExpression = definitionExpression;
-                    view.goTo(view.center);
+                var definitionExpression = `Name = 'N${eventId}'`;
+                mucDoVungNgap_IL.definitionExpression = definitionExpression;
+                tuyenDuongNgap_IL.renderer.valueExpression = getExpressionValues(`'D${eventId}'`);
+                view.goTo(view.center);
             });
         }
 
